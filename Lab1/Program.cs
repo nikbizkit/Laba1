fe4e90852e8638e2d03f6e40b9f99e74f832b57a
nikbizkit <43748053+nikbizkit@users.noreply.github.com>
2018-12-24 07:44:20 +0300

    Add files via upload

---
+++ Lab1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = GetNumber("A");
+            var b = GetNumber("B");
+            var c = GetNumber("C");
+            
+            var d = b * b - 4 * a * c;
+            if (d < 0)
+            {
+                Console.WriteLine($"Уравнение {a}x^2 + {b}x + {c} = 0 не имеет действительных корней");
+                return;
+            }
+
+            var sqrtD = Math.Sqrt(d);
+            var twoA = a + a;
+
+            if (d > 0)
+            {
+                var x1 = (-b - sqrtD) / twoA;
+                var x2 = (-b + sqrtD) / twoA;
+                Console.WriteLine($"Уравнение {a}x^2 + {b}x + {c} = 0 имеет 2 действительных корня: " +
+                                  $"x1 = {x1}, x2 = {x2}");
+                return;
+            }
+
+            var x = -b / twoA;
+            Console.WriteLine($"Уравнение {a}x^2 + {b}x + {c} = 0 имеет 2 совпадающих действительных корня: x = {x}");        
+	} 
+
+        static double GetNumber(string coef)
+        {
+            do
+            {
+                Console.WriteLine($"Введите коэффициент {coef}: ");
+                var success = double.TryParse(Console.ReadLine(), out var num);
+                if (success)
+                {
+                    return num;
+                }
+                Console.WriteLine("Ошибка. Введите действительное число.\n");
+            } while (true);
+        }
+    }
+}
